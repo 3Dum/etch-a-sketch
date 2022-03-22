730fefb7e8971dd6cf5163baf6a78b8c8d63b213
Luke <luke.goddard44@gmail.com>
2022-03-22 16:04:06 +1300

    Hover over will change color of squares

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const container = document.querySelector('.container');
 
+function changeColor(e) {
+  this.style.backgroundColor = 'green';
+}
+
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const square = document.createElement('div');
     square.id = i * 16 + j;
     square.classList.add('square');
+    square.addEventListener('mouseover', changeColor);
     container.appendChild(square);
   }
 }

7aa881f1e3fa15f6374725ecd469f3d192e00683
Luke <luke.goddard44@gmail.com>
2022-03-22 16:43:20 +1300

    Add grid resizing functionality on click

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+
+button.addEventListener('click', onClick);
+
+function onClick() {
+  let gridDimension;
+  do {
+    gridDimension = prompt('Enter grid dimensions');
+  } while (!Number.isInteger(+gridDimension));
+  drawGrid(gridDimension);
+}
 
 function changeColor(e) {
   this.style.backgroundColor = 'green';
 }
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement('div');
-    square.id = i * 16 + j;
-    square.classList.add('square');
-    square.addEventListener('mouseover', changeColor);
-    container.appendChild(square);
+function drawGrid(dimensions=16) {
+  container.innerHTML = '';
+  container.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${dimensions}, 1fr)`;
+
+  for (let i = 0; i < dimensions; i++) {
+    for (let j = 0; j < dimensions; j++) {
+      const square = document.createElement('div');
+      square.id = i * dimensions + j;
+      square.classList.add('square');
+      square.addEventListener('mouseover', changeColor);
+      container.appendChild(square);
+    }
   }
 }
+
+drawGrid();
